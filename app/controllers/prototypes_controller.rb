de084185e3e8eed1acf5a5ea03986707f15701de
emi-sqbt <71768074+emi-sqbt@users.noreply.github.com>
2020-10-16 16:14:48 +0900

    ProtoSpace挙動確認修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -27,7 +27,7 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-    unless current_user == @prototype.user_id
+    unless current_user == @prototype.user
       redirect_to root_path
     end
   end
